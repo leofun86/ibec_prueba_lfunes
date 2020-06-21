4ae7dbbe882c7e900032220c47886e545056eff8
Leo Funes <leorecord@hotmail.com>
2020-06-21 00:27:11 -0300

    Creación de objeto con id, objeto producto && objeto cliente

--- stockApp/server/mostrar_stock.php
+++ stockApp/server/mostrar_stock.php
@@ -27,7 +27,30 @@
 
       if ($result->num_rows > 0) {
         $data=[];
-        while($regs = $result->fetch_array()) { $data[]=$regs; }
+        $producto=[];
+        $cliente=[];
+        while($regs = $result->fetch_array()) { 
+          //Datos del producto
+          $request_productos=$mysqli->prepare('SELECT * FROM productos WHERE id_producto="'.$regs['id_producto'].'"');
+          $request_productos->execute(); $result_productos=$request_productos->get_result();
+          while($regs_prod = $result_productos->fetch_assoc()) { $producto=$regs_prod; }
+          //Datos del cliente
+          $request_clientes=$mysqli->prepare('SELECT * FROM clientes WHERE ci="'.$regs['ci_cliente'].'"');
+          $request_clientes->execute(); $result_clientes=$request_clientes->get_result();
+          while($regs_cli = $result_clientes->fetch_assoc()) { $cliente=$regs_cli; }
+
+          $data[]=array(
+            'id'=> $regs['id_producto'],
+            "producto" => $producto,
+            "cliente" => $cliente,
+          );
+        }
+        //echo $cliente;
+        //echo '<br>----------------------';
+        //echo '<br>----------------------<br>';
+        //echo $producto;
+        //echo '<br>----------------------';
+        //echo '<br>----------------------<br>';
         echo json_encode($data);
       } else { echo json_encode(array('error' => true)); }
       $new_request->close();
